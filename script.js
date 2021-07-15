56e68f20674b68fe847b5f628901844a013bf770
gabrielfontineli <gabriel0fontineli@gmail.com>
2021-07-15 13:54:25 -0300

    adjusting code

--- script.js
+++ script.js
@@ -3,11 +3,7 @@
 var formulario = {};
 (function(formulario){
     formulario.cpf = document.getElementById('cpf');
-    formulario.bairro = document.getElementById('bairro');
-    formulario.endereco = document.getElementById('endereco')
-    formulario.cidade = document.getElementById('cidade');
-    formulario.estado = document.getElementById('estado');
-    formulario.celular = document.getElementById('celular');
+    formulario.telefone = document.getElementById('telefone');
     formulario.required = document.querySelectorAll(".required");
     formulario.invalidElement = document.querySelector("span");
 })(formulario);
@@ -37,79 +33,6 @@ function validatecpf(cpf) {
 
     return result != entry.charAt(1) ? false : true
 }
-
-function validate() {
-    const cpf = formulario.cpf.value.replace(/[^a-z0-9 ]/g, "")
-    const requiredInputs = formulario.required;
-    let inputIsValid = false;
-    const cpfIsValid = validatecpf(cpf);
-
-    for (let i = 0; i < requiredInputs.length; i++) {
-        if (requiredInputs[i].value === "")  {
-            requiredInputs[i].style["border"] = "1px solid red"
-            inputIsValid = false;
-        } else {
-            inputIsValid = true
-            requiredInputs[1].style["border"] = "none"
-            formulario.invalidElement.style["display"] = "none"
-            requiredInputs[i].style["border"] = "none"
-        }
-    }
-
-    if (!inputIsValid || !cpfIsValid) {
-        window.alert("Opss! Aconteceu um erro no seu cadastro, verifique se você preencheu todos espaços necessários")
-        cpfIsValid ? (
-            requiredInputs[1].style["border"] = "none",
-            formulario.invalidElement.style["display"] = "none"
-        ) : (
-            requiredInputs[1].style["border"] = "1px solid red",
-            formulario.invalidElement.style["display"] = "inline"
-        );
-        return false
-    }
-    else if (cpfIsValid && inputIsValid) window.alert("Parabéns!! você foi cadastrado com sucesso")
-    return true
-}
-function clearCepInputs() {
-    formulario.endereco.value = ("");
-    formulario.bairro.value = ("");
-    formulario.cidade.value = ("");
-    dformulario.estado.value = ("");
-}
-
-function retrievingCepInputs(cepInputs) {
-    if ('erro' in cepInputs) {
-        clearCepInputs();
-        alert("CEP não encontrado.");
-        return;
-    }
-    formulario.endereco.value = (cepInputs.logradouro);
-    formulario.bairro.value = (cepInputs.bairro);
-    formulario.cidade.value = (cepInputs.localidade);
-    formulario.estado.value = (cepInputs.uf);
-}
-
-function searchCep(valor) {
-
-    const cep = valor.replace(/\D/g, '');
-
-    if (cep === "")  {
-        clearCepInputs(); 
-        return;
-    }
-
-    const validateCep = /^[0-9]{8}$/;
-    if (! validateCep.test(cep)) {
-        clearCepInputs();
-        alert("Formato de CEP inválido.");
-        return;
-    } 
-
-    const script = document.createElement('script');
-    script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=retrievingCepInputs';
-    document.body.appendChild(script);
-
-};
 
 function cpfMask() {
     const length = formulario.cpf.value.length;
